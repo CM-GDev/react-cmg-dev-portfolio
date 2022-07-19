4d7f45b74343dae1c99b92c246a01ed03163ac6d
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-07-18 18:30:30 -0700

    added folder and page structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import profilePic from './img/Picture.jpg';
+import Navigation from "./components/Navigation";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React Portfolio Page</h1>
-      <h2>Author: Cristobal Marquez-Glynn</h2>
-      <img src={profilePic} alt="profile" />
-    </div>
-  );
-}
+const App = () => <Navigation />;
+
 
 export default App;
