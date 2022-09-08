242bb34c5777a88723e43fac0a4bf8976f24d88b
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-09-08 13:02:07 -0700

    updated resume and about me section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -7,7 +7,7 @@ export default function AboutMe() {
     <main className='container'>
       <h3 className="blue-text text-darken-1">About Me</h3>
       <p>
-      I'm a full stack web developer applying over ten years of experience as a mechanical engineer in the Aerospace and Defense industries to build robust and efficient web applications. I'm currently enrolled in a full stack web developer course through the University of Washington and am developing skills and applying industry best practices in JavaScript, HTML, and CSS to develop and support responsive web designs.</p>
+      I'm a full stack web developer applying over ten years of experience as a mechanical engineer to build robust and efficient web applications. Recently completed a full stack web developer bootcamp through the University of Washington. In the course I learned and applied industry best practices in JavaScript, HTML, and CSS to develop and support responsive web designs.</p>
       <p> My previous experience as a project engineer extended my technical and non-technical skills critical to leading teams through multi-year long mechanical design and qualification projects from inception to completion. In my current web application projects, I continue to leverage thorough planning by approaching big and small project milestones with equal importance in an agile software development environment. Passionate about learning and excited to join a team to provide technical and long lasting web application solutions.
       </p>
     </main>
